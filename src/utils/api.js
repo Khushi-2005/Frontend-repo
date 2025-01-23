700fe5bfd0ebcab850f946fbd02ef8fc995da862
Khushi-2005 <104870133+Khushi-2005@users.noreply.github.com>
2025-01-24 00:33:42 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
+  baseURL: "https://backend-repo-eq0i.onrender.com/api",
 });
 
 export const getAllProperties = async () => {
