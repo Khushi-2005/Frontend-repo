009f2aa693e2cd02bd4268b13082604c2e33e502
Khushi Upadhyay <khushiupadhyay@Khushis-Laptop.local>
2025-01-23 22:46:08 +0530

    Initial commit for Client

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-3hh4igq2dpyla40a.us.auth0.com"
+     clientId="FyT2Jj0gFVAvhSxDQYiyONuJuZK31ZDH"
+     authorizationParams={{
+      redirect_uri: "http://localhost:5173/"
+     }}
+     audience="http://localhost:3000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
